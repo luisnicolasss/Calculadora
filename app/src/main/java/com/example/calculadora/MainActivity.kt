0b6f488ed072a002662f21c7f09883ebd11d56c8
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-07-14 12:29:39 -0300

    Añadir texto con append al tvOperation

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun onClickButton(view: View){
+        val valueStr = (view as Button).text.toString()
+
+        when(view.id){
+            R.id.btnDelete -> {
+
+            }
+            R.id.btnClear -> {
+
+            }
+            R.id.btnResolve -> {
+
+            }
+            else -> {
+               binding.tvOperation.append(valueStr)
+            }
+        }
     }
 }
